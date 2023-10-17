f84943235b70ca92ddae2b00a6f5b36a2aad9958
Jonathan Gobiel <jonathangobiel13@gmail.com>
2023-10-17 11:51:07 +0700

    testing

--- .github/actions/bundle-size/index.js
+++ .github/actions/bundle-size/index.js
@@ -5,11 +5,12 @@ const approotdir = require("app-root-dir");
 const core = require("@actions/core");
 
 const { bundleSizeOutput } = bundler.readReportsText(result);
+
+core.debug("1231");
+
 const filename = "bundle-size-report.txt";
 
 const result = fs.readFileSync(path.join(approotdir.get(), filename), "utf-8");
-
-console.log("1231");
 
 const sizeMap = bundler.getSizeMap(bundleSizeOutput);
 
