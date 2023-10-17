0c364827ae83f9b024cdf4ecc4393566ee736f8b
Jonathan Gobiel <jonathangobiel13@gmail.com>
2023-10-17 15:51:00 +0700

    testing -33

--- .github/actions/format-bundle-size-report/index.js
+++ .github/actions/format-bundle-size-report/index.js
@@ -2,14 +2,17 @@ const bundler = require("./utils");
 const core = require("@actions/core");
 
 const bundleSizeMap = core.getInput("bundleSizeMap");
+const parsedBundleSizeMap = JSON.parse(bundleSizeMap);
+
 const bundleSizeStr = core.getInput("bundleSizeStr");
 
 const masterBundleSizeMap = core.getInput("masterBundleSizeMap");
+const parsedMasterBundleSizeMap = JSON.parse(masterBundleSizeMap);
 
 const commentMsg = bundler.constructCommentMessage(
   bundleSizeStr,
-  bundleSizeMap,
-  masterBundleSizeMap
+  parsedBundleSizeMap,
+  parsedMasterBundleSizeMap
 );
 
 core.setOutput("report", commentMsg);
