22c9a8a717d7c5776877d2ab2887e3f3bb0efd11
Jonathan Gobiel <jonathangobiel13@gmail.com>
2023-10-17 08:52:56 +0700

    change output master

--- .github/actions/format-bundle-size-report/index.js
+++ .github/actions/format-bundle-size-report/index.js
@@ -2,9 +2,16 @@ const bundler = require("./utils");
 const core = require("@actions/core");
 
 const bundleSizeOutput = core.getInput("bundleSizeOutput");
+const masterBundleSize = core.getInput("masterBundleSize");
 
 const sizeMap = bundler.getSizeMap(bundleSizeOutput);
-const masterBundleSize = core.getInput("masterBundleSize");
+
+if (!sizeMap) {
+  core.setFailed(
+    "💥 Failed to generate `sizeMap` from `npx @wpe-tkpd/bundlesize` output"
+  );
+  return null;
+}
 
 const commentMsg = bundler.constructCommentMessage(
   bundleSizeOutput,
